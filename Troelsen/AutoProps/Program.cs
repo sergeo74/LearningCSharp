ae25a2fdfddcf514e127c48932a4639452e4301d
Сергей Нехаенко <sergeo74@outlook.com>
2019-10-08 20:16:33 +0300

    class Car

--- Troelsen/AutoProps/Program.cs
+++ Troelsen/AutoProps/Program.cs
@@ -10,6 +10,31 @@ namespace AutoProps
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun With Automatic Properties *****");
+            Car car = new Car("LADA", 55, "Red");
+            car.DisplayStats();
+        }
+    }
+
+    class Car
+    {
+        public string  PetName { get; set; }
+        public int Speed { get; set; }
+        public string  Color { get; set; }
+        public Car() { }
+        public Car(string name, int speed, string color)
+        {
+            PetName = name;
+            Speed = speed;
+            Color = color;
+        }
+        public void DisplayStats()
+        {
+            Console.WriteLine("Car Name:{0}", PetName);
+            Console.WriteLine("Speed:{0}", Speed);
+            Console.WriteLine("Color:{0}", Color);
+            Console.ReadLine();
+
         }
     }
 }
