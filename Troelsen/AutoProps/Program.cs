2e9951946e2cf13960b533c6ed8156992e162e7a
Сергей Нехаенко <sergeo74@outlook.com>
2019-10-09 11:05:38 +0300

    End AutoProps

--- Troelsen/AutoProps/Program.cs
+++ Troelsen/AutoProps/Program.cs
@@ -10,6 +10,12 @@ namespace AutoProps
             Console.WriteLine("***** Fun With Automatic Properties *****");
             Car car = new Car("LADA", 55, "Red");
             car.DisplayStats();
+            Console.ReadLine();
+            Garage g = new Garage();
+            g.MyAuto = car;
+            Console.WriteLine("Number of Cars in Garage: {0}", g.NumberOfCar);
+            Console.WriteLine("Name of My Car in Garage: {0}", g.MyAuto.PetName);
+            Console.ReadLine();
         }
     }
 
@@ -30,8 +36,18 @@ namespace AutoProps
             Console.WriteLine("Car Name:{0}", PetName);
             Console.WriteLine("Speed:{0}", Speed);
             Console.WriteLine("Color:{0}", Color);
-            Console.ReadLine();
-
+            
+        }
+    }
+    class Garage
+    {
+        public int NumberOfCar { get; set; } = 1;
+        public Car MyAuto { get; set; } = new Car();
+        public Garage() {}
+        public Garage(int number, Car car)
+        {
+            NumberOfCar = number;
+            MyAuto = car;
         }
     }
 }
