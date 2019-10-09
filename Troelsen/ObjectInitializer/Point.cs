a5889853428196d3cf91b869bf98c413a02f892e
Сергей Нехаенко <sergeo74@outlook.com>
2019-10-09 12:02:47 +0300

    Fun With Object Init Syntax

---
+++ Troelsen/ObjectInitializer/Point.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace ObjectInitializer
+{
+    enum PointColor {LightBlue, BlueRed, Gold }
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public PointColor Color { get; set; }
+        public Point(int xVal, int yVal)
+        {
+            X = xVal;
+            Y = yVal;
+            Color = PointColor.Gold;
+        }
+        public Point(PointColor ptColor)
+        {
+            Color = ptColor;
+        }
+        public Point() : this(PointColor.BlueRed) { }
+        public void DisplayStats()
+        {
+            System.Console.WriteLine("[{0},{1}, {2}]", X,Y, Color);
+        }
+    }
+}
---
+++ Troelsen/ObjectInitializer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace ObjectInitializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun With Object Init Syntax *****");
+            //Устанвливаем свойства вручную
+            Point firstPoint = new Point();
+            firstPoint.X = 10;
+            firstPoint.Y = 10;
+            firstPoint.DisplayStats();
+            //Устанвливаем свойства специальным конструктором
+            Point secondPoint = new Point(20, 20);
+            secondPoint.DisplayStats();
+            //Устанвливаем свойства используя синтаксис инициализации
+            Point thirdPoint = new Point { Y = 30, X = 30 };
+            thirdPoint.DisplayStats();
+
+            Point goldPoint = new Point (PointColor.Gold) { Y = 90, X = 60 };
+            goldPoint.DisplayStats();
+
+            Console.ReadLine();
+
+        }
+    }
+}
