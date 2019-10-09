cc2eb07198c0459a6db6ab051902ca95505a5e95
Сергей Нехаенко <sergeo74@outlook.com>
2019-10-09 16:06:27 +0300

    End Chapter 5

---
+++ Troelsen/ConstData/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace ConstData
+{
+    class MyMathClass
+    {
+        public const double PI = 3.14;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
