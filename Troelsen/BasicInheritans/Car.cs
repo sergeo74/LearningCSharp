551e72fc79111828f169f0cf6dc7646a6b2eb548
Сергей Нехаенко <sergeo74@outlook.com>
2019-10-10 16:58:11 +0300

    p.245

---
+++ Troelsen/BasicInheritans/Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace BasicInheritans
+{
+    class Car
+    {
+        public readonly int maxSpeed;
+        private int currSpeed;
+        public int Speed
+        {
+            get
+            {
+                return currSpeed;
+            }
+
+            set
+            {
+                currSpeed = value;
+                if (currSpeed > maxSpeed)
+                {
+                    currSpeed = maxSpeed;
+                }
+            }
+        }
+        public Car()
+        {
+            maxSpeed = 55;
+        }
+
+        public Car(int max)
+        {
+            maxSpeed = max;
+        }
+    }
+    class MiniVan : Car
+    {
+
+    }
+}
---
+++ Troelsen/BasicInheritans/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BasicInheritans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Basic Inheritance *****");
+            Car car = new Car(80); //{ Speed= 100};
+            car.Speed = 150;
+            Console.WriteLine($"My car is going {car.Speed} MPH");
+            MiniVan miniVan = new MiniVan();
+            miniVan.Speed = 100;
+            Console.WriteLine($"My van is going {miniVan.Speed} MPH");
+            Console.ReadLine();
+        }
+    }
+   
+}
