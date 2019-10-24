99e45871b04a1693910eaf4677e1c2c7da634ce0
Сергей Нехаенко <sergeo74@outlook.com>
2019-10-24 17:00:16 +0300

    Exeptions p.285

---
+++ Troelsen/ObjectOverrides/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ObjectOverrides
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun With System.Object *****");
+            Person person = new Person("Sergeo","Next",45);
+            Person person1 = new Person("Sergeo", "Next", 45);
+           // person1.Age = 40;
+            Console.WriteLine("person.ToString(): {0}", person.ToString());
+            Console.WriteLine("person1.ToString(): {0}", person1.ToString());
+
+            //Тестируем Equals
+            Console.WriteLine("person = person1 ? :{0}", person.Equals(person1));
+            //Проверяем хэш коды
+            Console.WriteLine("Same hash codes ? :{0}", person.GetHashCode()== person1.GetHashCode());
+            //Эквивалентность
+            Console.WriteLine("Poiting to same object ? :{0}", object.ReferenceEquals(person,person1));
+            Console.ReadLine();
+        }
+    }
+    class Person 
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Person() {}
+        public Person(string fName, string lName, int age)
+        {
+            FirstName = fName;
+            LastName = lName;
+            Age = age;
+        }
+        public override string ToString()
+        {
+            return $"[First Name: {FirstName}; Last Name: {LastName}; Age: {Age}]";
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is Person && obj != null)
+            {
+                Person temp;
+                temp = (Person)obj;
+                if (temp.FirstName == this.FirstName && temp.LastName == this.LastName &&
+                    temp.Age == this.Age)
+                {
+                    return true;
+                }
+                else 
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+
+    }
+}
---
+++ Troelsen/SimpleExeption/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace SimpleExeption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Car
+    {
+        public const int MaxSpeed = 100;
+        public int CurrentSpeed { get; set; } = 0;
+        public string PetName { get; set; } = "";
+        //Автомобиль не сломался?
+        bool carIdDead;
+        //Есть радиоприёмник
+        Radio radio = new Radio();
+        //Конструктры
+        public Car(){}
+        public Car(string name, int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+        //Метод делегирует запрос к Radio
+        public void CranckTunes(bool state)
+        {
+            radio.TurnOn(state);
+        }
+        //Прибавим скорость, если не перегрелся
+        public void Accelerate(int delta)
+        {
+            if (carIdDead)
+                Console.WriteLine("{0} is out of order...", PetName);
+            else
+            {
+                CurrentSpeed += delta;
+                if (CurrentSpeed > MaxSpeed)
+                {
+                    Console.WriteLine("{0} is overheated!", PetName);
+                    CurrentSpeed = 0;
+                    carIdDead = true;
+                }
+                else
+                {
+                    Console.WriteLine("CurrentSpeed = {0} ", CurrentSpeed);
+                }
+            }
+        }
+    }
+    class Radio
+    {
+        public void TurnOn(bool on)
+        {
+            Console.WriteLine(on ? "Jamming...":"Quiet time...");
+        }
+    }
+}
