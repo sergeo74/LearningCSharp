eebbd88a01fca494353b5b3528d4c5bcb9f8064d
Сергей Нехаенко <sergeo74@outlook.com>
2019-11-25 17:29:57 +0300

    from RD

---
+++ Troelsen/ExtensionMethods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Extension Methods *****\n");
+            int myInt = 123456;
+            //MyExtensions.DisplayDefiningAssembly(myInt);
+            Console.WriteLine(myInt.ReverseDigits().ToString());
+            myInt.DisplayDefiningAssembly();
+            Console.ReadKey();
+            
+            // To же и в DataSet!
+            System.Data.DataSet d = new System.Data.DataSet();
+            d.DisplayDefiningAssembly();
+            Console.ReadKey();
+            
+            //Ив SoundPlayer!
+            System.Media.SoundPlayer sp = new System.Media.SoundPlayer();
+            sp.DisplayDefiningAssembly();
+            Console.ReadKey();
+        }
+    }
+}
