7c9f80dfb8b54f6197d08e79e01cc0caf913f716
Сергей Нехаенко <sergeo74@outlook.com>
2019-11-29 15:21:24 +0300

    LINQ:End

---
+++ Troelsen/FunWithLinqExpressions/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Linq;
+
+namespace FunWithLinqExpressions
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("******* Fun with Query Expressions *****\n");
+            
+            // Этот массив будет основой для тестирования...
+            ProductInfo[] itemsInStock = new[]
+            {
+                new ProductInfo
+                {
+                    Name = "Mac's Coffee",
+                    Description = "Coffee with TEETH",
+                    NumberInStock = 24
+                },
+                new ProductInfo
+                {
+                    Name = "Milk Maid Milk",
+                    Description = "Milk cow's love",
+                    NumberInStock = 100
+                },
+                new ProductInfo
+                {
+                    Name = "Pure Silk Tofu",
+                    Description = "Bland as Possible",
+                    NumberInStock = 120
+                },
+                new ProductInfo
+                {
+                    Name = "Crunchy Pops",
+                    Description = "Cheezy, peppery goodness",
+                    NumberInStock = 2
+                },
+                new ProductInfo
+                {
+                    Name = "RipOff Water",
+                    Description = "From the tap to your wallet",
+                    NumberInStock = 24
+                },
+                new ProductInfo
+                {
+                    Name = "Classic Valpo Pizza",
+                    Description = "Everyone loves pizza!",
+                    NumberInStock = 73
+                },
+            };
+            
+            // Здесь мы будем вызывать разнообразные методы!
+            SelectEverything(itemsInStock);
+            ListProductNames(itemsInStock);
+            GetOverstock(itemsInStock);
+            GetNamesAndDescriptions(itemsInStock);
+            Console.ReadLine();
+        }
+
+        static void SelectEverything(ProductInfo[] products)
+        {
+            var allProducts = from product in products select product;
+            foreach (var prod in allProducts)
+            {
+                Console.WriteLine(prod.ToString ()); 
+            }
+        }
+        static void ListProductNames(ProductInfo[] products)
+        {
+            // Теперь получить только наименования товаров.
+            Console.WriteLine("Only product names:");
+            var names = from p in products select p.Name;
+            foreach (var n in names)
+            {
+                Console.WriteLine("Name: {0}", n) ;
+            }
+        }
+
+        static void GetOverstock(ProductInfo[] products)
+        {
+            Console.WriteLine("The overstock items!");
+            // Получить только товары co складским запасом более 25 единиц.
+            var overstock = from p in products where p.NumberInStock > 25 select p;
+            foreach (var c in overstock)
+            {
+                Console.WriteLine(c.ToString());
+            }
+        }
+        static void GetNamesAndDescriptions(ProductInfo[] products)
+        {
+            Console.WriteLine("Names and Descriptions:");
+            var nameDesc = from p in products select new { p.Name, p.Description };
+            foreach (var item in nameDesc)
+            {
+                // Можно было бы также использовать свойства Name и Description напрямую.
+                Console .WriteLine (item.ToString());
+            }
+        }
+    }
+
+    class ProductInfo
+    {
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public int NumberInStock { get; set; } = 0;
+        public override string ToString()
+        {
+            return $"Name = {Name}, Description = {Description}, " +
+                   $"Number in Stock = {NumberInStock}";
+        }
+    }
+}
---
+++ Troelsen/LinqUsingEnumerable/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using System.Security.Policy;
+
+namespace LinqUsingEnumerable
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            QueryStringWithOperators();
+            QueryStringsWithEnumerableAndLambdas();
+            QueryStringsWithAnonymousMethods();
+            VeryComplexQueryExpression.QueryStringsWithRawDelegates();
+        }
+        static void QueryStringWithOperators()
+        {
+            Console.WriteLine("***** Using Query Operators *****");
+            string[] currentVideoGames = {"Morrowind", "Uncharted 2", "Fallout 3", 
+                "Daxter", "System Shock 2"};
+            var subset = from game in currentVideoGames where game.Contains(" ")
+                orderby game select game;
+            foreach (string g in subset)
+            {
+                Console.WriteLine("Item: {0}",g );
+            }
+
+            Console.WriteLine();
+        }
+        static void QueryStringsWithEnumerableAndLambdas()
+        {
+            Console.WriteLine("***** Using Enumerable / Lambda Expressions *****");
+            string[] currentVideoGames = {"Morrowind", "Uncharted 2", "Fallout 3", 
+                "Daxter", "System Shock 2"};
+            var subset = currentVideoGames.Where(game => game.Contains(" "))
+                .OrderBy(game => game).Select(game => game);
+            foreach (var g in subset)
+            {
+                Console.WriteLine("Item: {0}",g );
+            }
+
+            Console.WriteLine();
+        }
+
+        static void QueryStringsWithAnonymousMethods()
+        {
+            Console.WriteLine("***** Using Anonymous Methods *****");
+            string[] currentVideoGames = {"Morrowind", "Uncharted 2", "Fallout 3", 
+                "Daxter", "System Shock 2"};
+            // Построить необходимые делегаты Funco с использованием анонимных методов.
+            Func<string, bool> searchFilter = delegate(string game) 
+                { return game.Contains(" ");};
+            Func<string, string> itemToProcess = delegate(string s) { return s;};
+            // Передать делегаты в методы класса Enumerable.
+            var subset = currentVideoGames.Where(searchFilter).OrderBy(itemToProcess)
+                .Select(itemToProcess);
+            foreach (var g in subset)
+            {
+                Console.WriteLine("Item: {0}",g );
+            }
+
+        }
+    }
+    class VeryComplexQueryExpression
+    {
+        public static void QueryStringsWithRawDelegates()
+        {
+            Console.WriteLine("***** Using Raw Delegates *****");
+            string[] currentVideoGames = {"Morrowind", "Uncharted 2", "Fallout 3", 
+                "Daxter", "System Shock 2"};
+            // Построить необходимые делегаты Func<>.
+            Func<string, bool> searchFilter = new Func<string, bool>(Filter) ;
+            Func<string, string> itemToProcess = new Func<string, string> (Processltem);
+            var subset =currentVideoGames.Where(searchFilter).OrderBy(itemToProcess)
+                    .Select(itemToProcess);
+            
+            foreach (var g in subset)
+            {
+                Console.WriteLine("Item: {0}",g );
+            }
+        }
+        // Цели делегатов.
+        static bool Filter(string game) => game.Contains(" ");
+        static string Processltem(string game) => game;
+    }
+    
+}
