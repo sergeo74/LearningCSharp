80bd5e7eca2bfb17e64fc1137a06df892b4238f0
Сергей Нехаенко <sergeo74@outlook.com>
2019-12-16 16:38:17 +0300

    Using attributes

---
+++ Troelsen/ApplyingAttributes/HorseAndBuggy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplyingAttributes
+{
+    [Serializable]
+    [Obsolete("Use another vehicle!")]
+    class HorseAndBuggy
+    {
+    }
+}
---
+++ Troelsen/ApplyingAttributes/Motorcycle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplyingAttributes
+{
+    [Serializable]
+    class Motorcycle
+    {
+        [NonSerialized]
+        float weightOfCurrentPassengers;
+        // Эти поля остаются сериализируемыми.
+        bool hasRadioSystem;
+        bool hasHeadSet;
+        bool hasSissyBar;
+    }
+}
