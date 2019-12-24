c8c95dcde3ea3abc6146755b07512efd025a64e1
Сергей Нехаенко <sergeo74@outlook.com>
2019-12-24 15:37:23 +0300

    Domein creation. p.637

---
+++ Chapter 17.Processes and Domeins/DefaultAppDomainApp/DefaultAppDomainApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefaultAppDomainApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with the default AppDomain *****\n");
+            InitDAD();
+           // DisplayDADStats();
+            ListAHAssembliesInAppDomain();
+            
+            Console.ReadLine();
+        }
+
+        private static void DisplayDADStats()
+        {
+            AppDomain defaultAD = System.AppDomain.CurrentDomain;
+            Console.WriteLine("Name of this domain: {0}", defaultAD.FriendlyName);
+            Console.WriteLine("ID of domain in this process: {0}", defaultAD.Id);
+            Console.WriteLine("Is this the default domain?: {0}", defaultAD.IsDefaultAppDomain());
+            Console.WriteLine("Base directory of this domain: {0}", defaultAD.BaseDirectory);
+        }
+        private static void ListAHAssembliesInAppDomain()
+        {
+            AppDomain defaultAD = System.AppDomain.CurrentDomain;
+            //Assembly[] loadedAssemblies = defaultAD.GetAssemblies();
+            var loadedAssemblies = from a in defaultAD.GetAssemblies()
+                                   orderby a.GetName().Name
+                                   select a;
+            Console.WriteLine("\n***** Here are the assemblies loaded in {0} *****\n",
+                defaultAD.FriendlyName);
+            foreach(var asm in loadedAssemblies)
+            {
+                Console.WriteLine("-> Name: {0}", asm.GetName().Name);
+                Console.WriteLine("-> Version: {0}", asm.GetName().Version);
+            }
+        }
+
+        private static void InitDAD()
+        {
+            // Эта логика будет выводить имя любой сборки, загруженной
+            //в домен приложения после его создания.
+            AppDomain defaultAD = AppDomain.CurrentDomain;
+            defaultAD.AssemblyLoad += (o, s) => 
+            {
+                Console.WriteLine("{0} has been loaded!", s.LoadedAssembly.GetName().Name);
+            };
+        }
+
+    }
+}
--- Chapter 17.Processes and Domeins/ProcessManipulator/ProcessManipulator/Program.cs
+++ Chapter 17.Processes and Domeins/ProcessManipulator/ProcessManipulator/Program.cs
@@ -10,8 +10,16 @@ namespace ProcessManipulator
         {
             Console.WriteLine("***** Fun With Processes *****\n");
             ListAllRunningProcesses();
+            //GetSpecificProcess();
+
+            // Запросить у пользователя PID и вывести набор активных потоков.
+            Console.WriteLine("***** Enter PID of process to investigate *****");
+            string pID = Console.ReadLine();
+            int theProcID = int.Parse(pID);
+            EnumThreadsForPid(theProcID);
+            EnumModsForPid(theProcID);
             Console.ReadLine();
-            GetSpecificProcess();
+            StartAndKillProcess();
         }
 
         static void ListAllRunningProcesses()
@@ -37,6 +45,88 @@ namespace ProcessManipulator
                 Console.WriteLine(process.ProcessName);
             }
             catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                //throw;
+            }
+            Console.ReadLine();
+        }
+
+        static void EnumThreadsForPid( int pID)
+        {
+            Process theProc;
+            try
+            {
+                theProc = Process.GetProcessById(pID);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+                //throw;
+            }
+
+            // Вывести статистические сведения по каждому потоку в указанном процессе.
+            Console.WriteLine("Here are the threads used by: {0}", theProc.ProcessName);
+            
+            ProcessThreadCollection theThreads = theProc.Threads;
+            foreach (ProcessThread processThread in theThreads)
+            {
+                string info =
+                    $"-> Thread ID: {processThread.Id}" +
+                    $"\tStart Time: {processThread.StartTime.ToShortTimeString()}" +
+                    $"\tPriority: {processThread.PriorityLevel}";
+                Console.WriteLine(info);
+            }
+            Console.WriteLine("************************************");
+        }
+
+        static void EnumModsForPid(int pID)
+        {
+            Process theProc;
+            try
+            {
+                theProc = Process.GetProcessById(pID);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+                //throw;
+            }
+
+            // Вывести статистические сведения по каждому модулю в указанном процессе.
+            Console.WriteLine("\nHere are the loaded modules for:: {0}", theProc.ProcessName);
+
+            ProcessModuleCollection  theModules = theProc.Modules;
+            foreach (ProcessModule procMod in theModules)
+            {
+                string info =
+                    $"-> Module name: {procMod.ModuleName}";
+                Console.WriteLine(info);
+            }
+            Console.WriteLine("************************************");
+        }
+
+        static void StartAndKillProcess()
+        {
+            Process process = null;
+            try
+            {
+                process = Process.Start("taskmgr.exe");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+                //throw;
+            }
+            Console.WriteLine("Hit any key to kill opera");
+            Console.ReadLine();
+            try
+            {
+                process.Kill();
+            }
+            catch (InvalidOperationException ex)
             {
                 Console.WriteLine(ex.Message);
                 //throw;
