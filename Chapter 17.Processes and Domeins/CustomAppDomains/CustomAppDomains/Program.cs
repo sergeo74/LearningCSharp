41aef72b73c23817e756cc3a803e581fdadb88e1
Сергей Нехаенко <sergeo74@outlook.com>
2019-12-25 16:43:38 +0300

    Chapter 19

---
+++ Chapter 17.Processes and Domeins/CustomAppDomains/CustomAppDomains/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CustomAppDomains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with CustomAppDomains *****\n");
+            // Вывести все сборки, загруженные в стандартный домен приложения
+            AppDomain defaultAD = AppDomain.CurrentDomain;
+            defaultAD.ProcessExit += (o, s) =>
+             {
+                 Console.WriteLine("Default AD unloaded!");
+             };
+            ListAHAssembliesInAppDomain(defaultAD);
+
+            // Создать новый домен приложения.
+            MakeNewAppDomain();
+            Console.ReadLine();
+        }
+
+        private static void MakeNewAppDomain()
+        {
+            AppDomain newAD = AppDomain.CreateDomain("SecondAppDomain");
+            newAD.DomainUnload += (o, s) =>
+            {
+                Console.WriteLine("The second AppDomain has been unloaded!");
+            };
+            try
+            {
+                // Загрузить CarLibrary.dll в этот новый домен.
+                newAD.Load("CarLibrary");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                //throw;
+            }
+
+            ListAHAssembliesInAppDomain(newAD);
+
+            // Избавиться от этого домена приложения.
+            AppDomain.Unload(newAD);
+        }
+
+        private static void ListAHAssembliesInAppDomain(AppDomain ad)
+        {
+            var loadedAssemblies = from a in ad.GetAssemblies()
+                                   orderby a.GetName().Name
+                                   select a;
+            Console.WriteLine("\n***** Here are the assemblies loaded in {0} *****\n",
+                ad.FriendlyName);
+            foreach (var asm in loadedAssemblies)
+            {
+                Console.WriteLine("-> Name: {0}", asm.GetName().Name);
+                Console.WriteLine("-> Version: {0}", asm.GetName().Version);
+            }
+        }
+    }
+}
---
+++ Chapter 17.Processes and Domeins/ObgectContextApp/ObgectContextApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Threading;
+
+namespace ObgectContextApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Object Context *****\n");
+            SportsCar sport = new SportsCar();
+            Console.WriteLine();
+            SportsCar sport1 = new SportsCar();
+            Console.WriteLine();
+            SportsCarTS synchroSport = new SportsCarTS();
+            Console.ReadLine();
+        }
+    }
+
+   // Класс SportsCar не имеет никаких специальных
+    // контекстных потребностей и будет загружаться
+    // в стандартный контекст домена приложения,
+    class SportsCar
+    {
+        public SportsCar()
+        {
+            // Получить информацию о контексте и вывести идентификатор контекста.
+            Context ctx = Thread.CurrentContext;
+            Console.WriteLine("{0} object in context {1}", 
+                this.ToString(), ctx.ContextID);
+            foreach( IContextProperty itfCtxProp in ctx.ContextProperties)
+            {
+                Console.WriteLine("-> Ctx Prop: {0}", itfCtxProp.Name);
+            }
+        }
+    }
+
+    // SportsCarTS требует загрузки в синхронизированный контекст
+    [Synchronization]
+    class SportsCarTS: ContextBoundObject
+    {
+        public SportsCarTS()
+        {
+            // Получить информацию о контексте и вывести идентификатор контекста.
+            Context ctx = Thread.CurrentContext;
+            Console.WriteLine("{0} object in context {1}",
+                this.ToString(), ctx.ContextID);
+            foreach (IContextProperty itfCtxProp in ctx.ContextProperties)
+            {
+                Console.WriteLine("-> Ctx Prop: {0}", itfCtxProp.Name);
+            }
+        }
+    }
+}
