dd016cb9f62a09f8d03780b0fc8526b1e38765c0
Сергей Нехаенко <sergeo74@outlook.com>
2020-01-06 16:49:14 +0300

    Async

---
+++ Chapter 19. Threads/AsyncDelegate/AsyncDelegate/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace AsyncDelegate
+{
+    public delegate int BinaryOp(int x, int y);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Async Delegate Invocation *****");
+
+            // Вывести идентификатор выполняющегося потока.
+            Console.WriteLine("Main() invoked on thread {0}.",
+                Thread.CurrentThread.ManagedThreadId);
+
+            // Вызвать Add() во вторичном потоке.
+            BinaryOp b = new BinaryOp(Add);
+            IAsyncResult asyncResult = b.BeginInvoke(10, 10, null, null);
+
+            // Выполнить другую работу в первичном потоке...
+            Console.WriteLine("Doing more work in Main()!");
+
+            // По готовности получить результат выполнения метода Add().
+            int answ = b.EndInvoke(asyncResult);
+            Console.WriteLine("10 + 10 is {0}", answ);
+            Console.ReadLine();
+        }
+
+        static int Add(int x, int y)
+        {
+            // Вывести идентификатор выполняющегося потока.
+            Console.WriteLine("Add () invoked on thread {0}.",
+                Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(5000);
+            return x + y;
+        }
+    }
+}
---
+++ Chapter 19. Threads/SyncDelegateReview/SyncDelegateReview/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace SyncDelegateReview
+{
+    public delegate int BinaryOp(int x, int y);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Synch Delegate Review *****");
+
+            // Вывести идентификатор выполняющегося потока.
+            Console.WriteLine("Main() invoked on thread {0}.",
+                Thread.CurrentThread.ManagedThreadId);
+
+            // Вызвать Add() в синхронном режиме.
+            BinaryOp b = new BinaryOp(Add);
+
+            // Можно было бы также написать b.Invoke(10, 10);
+            int answ = b(10, 10);
+
+            // Эти строки кода не выполнятся до тех пор,
+            // пока не завершится метод Add().
+            Console.WriteLine("Doing more work in Main()!");
+            Console.WriteLine("10 + 10 is {0}", answ);
+            Console.ReadLine();
+        }
+        
+        static int Add(int x, int y)
+        {
+            // Вывести идентификатор выполняющегося потока.
+            Console.WriteLine("Add () invoked on thread {0}.",
+                Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(5000);
+            return x + y;
+        }
+    }
+}
