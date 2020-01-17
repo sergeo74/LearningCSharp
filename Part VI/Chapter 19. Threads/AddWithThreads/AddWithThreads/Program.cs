ad07e2dd4338240b88f50608b4a430a1a8c260fb
Сергей Нехаенко <sergeo74@outlook.com>
2020-01-17 15:42:25 +0300

    Parallel

---
+++ Part VI/Chapter 19. Threads/AddWithThreads/AddWithThreads/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace AddWithThreads
+{
+    class Program
+    {
+        private static AutoResetEvent waitHandle = new AutoResetEvent(false);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Adding with Thread objects *****");
+            Console.WriteLine("ID of thread in Add(): {0}",
+                     Thread.CurrentThread.ManagedThreadId);
+            AddParams ap = new AddParams(10, 20);
+            Thread thread1 = new Thread(new ParameterizedThreadStart(Add));
+            thread1.Start(ap);
+
+            // Ожидать, пока не поступит уведомление!
+            waitHandle.WaitOne();
+            Console.WriteLine("Other thread is done!");
+            Console.ReadLine();
+        }
+
+        static void Add(object data)
+        {
+            if (data is AddParams)
+            {
+                Console.WriteLine("ID of thread in Add(): {0}",
+                    Thread.CurrentThread.ManagedThreadId);
+                AddParams addParams = (AddParams)data;
+                Console.WriteLine("{0} + {1} is {2}",
+                    addParams.a, addParams.b, addParams.a+ addParams.b);
+                waitHandle.Set();
+            }
+        }
+    }
+
+    class AddParams
+        {
+            public int a, b;
+            public AddParams(int num1, int num2)
+            {
+                a = num1;
+                b = num2;
+            }
+        }
+
+}
---
+++ Part VI/Chapter 19. Threads/TimerАрр/TimerАрр/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace TimerАрр
+{
+    class Program
+    {
+        static void PrintTime(object state)
+        {
+            Console.WriteLine("Time is: {0}", DateTime.Now.ToLongTimeString());
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Working with Timer type *****\n");
+            TimerCallback timerCallback = new TimerCallback(PrintTime);
+            _ = new Timer(timerCallback, null, 0, 1000);
+            Console.WriteLine("Hit key to terminate...");
+            _ = Console.ReadLine();
+
+        }
+    }
+}
