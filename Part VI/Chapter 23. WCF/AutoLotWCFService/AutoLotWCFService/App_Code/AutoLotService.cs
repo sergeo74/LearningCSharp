4c0d03b25296976cbb37d92f285ffb4850f7142b
Сергей Нехаенко <sergeo74@outlook.com>
2020-04-10 15:12:49 +0300

    WCF END

---
+++ Part VI/Chapter 23. WCF/AutoLotWCFService/AutoLotWCFService/App_Code/AutoLotService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using AutoMapper;
+using AutoLotDAL.Models;
+using AutoLotDAL.Repos;
+using AutoLotDAL;
+
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class AutoLotService : IAutoLotService
+{
+	private Mapper MyMapper { get; set; }
+	public AutoLotService()
+	{
+		//Mapper.Initialize(cfg => cfg.CreateMap<Inventory, InventoryRecord>());
+		var conf = new MapperConfiguration(cfg => cfg.CreateMap<Inventory, InventoryRecord>());
+		MyMapper = new Mapper(conf);
+		
+	}
+
+	public void InsertCar(string make, string color, string petname)
+	{
+		var repo = new InventoryRepo();
+		repo.Add(new Inventory { Color = color, Make = make, PetName = petname });
+		repo.Dispose();
+	}
+	public void InsertCar(InventoryRecord car)
+	{
+		var repo = new InventoryRepo();
+		repo.Add(new Inventory { Color = car.Color, Make = car.Make, PetName = car.PetName });
+		repo.Dispose();
+	}
+	public List<InventoryRecord> GetInventory()
+	{
+		var repo = new InventoryRepo();
+		var records = repo.GetAll();
+		var results = MyMapper.Map<List<InventoryRecord>>(records);
+		repo.Dispose();
+		return results;
+	}
+	
+}
---
+++ Part VI/Chapter 23. WCF/AutoLotWCFService/AutoLotWCFService/App_Code/InventoryRecord.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+/// <summary>
+/// Summary description for InventoryRecord
+/// </summary>
+[DataContract]
+public class InventoryRecord
+{
+    [DataMember]
+    public int ID;
+
+    [DataMember]
+    public string Make;
+
+    [DataMember]
+    public string Color;
+
+    [DataMember]
+    public string PetName;
+
+
+}
